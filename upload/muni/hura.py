0f706095d69b97abf1804d8e15583b3e20982af9
Yaniv Pascal <yanivpas@gmail.com>
2015-03-05 23:28:53 +0000

    Updating the hura module

--- upload/muni/hura.py
+++ upload/muni/hura.py
@@ -1,6 +1,25 @@
 # TODO : create a db interface...
 
+from server.models import BudgetLine
+import csv
+
+fields = ['code', 'name', 'amount']
+
+MUNI = 'hura'
 
 def handle_sheet(year,filename):
+    # TODO: remove this. this is for debug perpesuse only.
+    BudgetLine.objects.all().delete()
+    reader = csv.DictReader(file(filename, 'rb'), fields)
+
+    for line in reader:
+        print "%s : %s" %(line['code'], line['amount'])
+        if (line['name'] != '' and 
+            line['amount'].isdigit()):
+           BudgetLine(name = line['name'],
+                      budget_id = line['code'],
+                      amount = line['amount'],
+                      year = year,
+                      muni = MUNI).save()
+        
     
-    return
