f465b0350c3f0086107b630232481bb3d28ba6e1
Yaniv Pascal <yanivpas@gmail.com>
2015-03-05 00:22:51 +0000

    Change the database to mongodb

--- settings.py
+++ settings.py
@@ -59,8 +59,8 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django_mongodb_engine',
+        'NAME': os.path.join(BASE_DIR, 'mongo_db'),
     }
 }
 
