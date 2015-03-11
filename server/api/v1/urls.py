3a979091dd188d710c9e8abf843fc9bd3b7a41be
mestrebisli <mestrebisli@gmail.com>
2015-03-11 21:33:20 +0200

    some typos in api

--- server/api/v1/urls.py
+++ server/api/v1/urls.py
@@ -1,11 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
-from openbudgets.api.v1.views import api_v1
-from openbudgets.apps.entities.urls.api import entities, divisions, domains
-from openbudgets.apps.sheets.urls.api import templates, sheets
-from openbudgets.apps.contexts.urls.api import contexts, coefficients
-from openbudgets.apps.accounts.urls import api as account_urls
-from openbudgets.apps.tools.urls import api as tool_urls
+from server.api.v1.views import api_v1
 
 
 urlpatterns = [
