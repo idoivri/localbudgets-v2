e0d64e831baa01f7fa01a83d935bc2f2c648b441
mestrebisli <mestrebisli@gmail.com>
2015-03-17 19:07:19 +0200

    Calling to API when searching

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -4,19 +4,20 @@ var jsonData = '['+
               '{"code":"3", "name":"בלה בלה","amnt":"54"}'+
             ']';
 
-var parseTableRes = (function(data,status){
-    var data = JSON.parse(jsonData);
-    var innerHtml ='';
-    var rowVal;
-    $.each(data, function(i,item){
-      rowVal = '<tr>'+
-                  '<td>' + item.code + '</td>' +
-                  '<td>' + item.name + '</td>' +
-                  '<td>' + item.amnt + '</td>' +
-                '</tr>';
-      innerHtml += rowVal;
-    });
-    $("#table_body").append(innerHtml);
+var parseTableRes = (function(data_res,status){
+    // var data = JSON.parse(data_res);
+    alert(data_res['res']);
+    // var innerHtml ='';
+    // var rowVal;
+    // $.each(data, function(i,item){
+    //   rowVal = '<tr>'+
+    //               '<td>' + item.code + '</td>' +
+    //               '<td>' + item.name + '</td>' +
+    //               '<td>' + item.amnt + '</td>' +
+    //             '</tr>';
+    //   innerHtml += rowVal;
+    // });
+    // $("#table_body").append(innerHtml);
 });
 
 
@@ -36,19 +37,16 @@ $(document).ready(function() {
         .sidebar('attach events', '.page.context .menu .item');
         // .sidebar('setting', 'transition', 'overlay');
   });
-  // parseTableRes();
   $("#search_btn").click(function(){
     // alert("wwor");
-    // var search_term = $("#search_general").text();
-    parseTableRes();
-    // $.get({
-    //   url: '',
-    //   type: 'GET',
-    //   data: {
-    //       city: ???
-    //   },
-    //   success: parseTableRes,
-    //   dataType: 'json'
-    // });
+    var search_term = $("#search_general").val();
+    alert(search_term);
+    $.get('/api/v1/lines',
+          {
+          code: search_term
+          },
+          parseTableRes,
+          'json'
+    );
   });
 });
