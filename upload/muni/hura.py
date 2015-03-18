e6408023d79744d985742ca5150199cdd7bee183
vagrant <vagrant@localbudgets>
2015-03-18 20:52:52 +0000

    Add class Dataset to upload.utils

--- upload/muni/hura.py
+++ upload/muni/hura.py
@@ -1,23 +1,18 @@
-# TODO : create a db interface...
-
-from pymongo import MongoClient
+#TODO : create a db interface...
+from upload.utils import Dataset
 import csv
 
 fields = ['code', 'name', 'amount']
 
 MUNI = 'hura'
 
-def handle_sheet(year,filename):
-    client = MongoClient()
-    db = client.database
-    muni = db[MUNI]
-    year_dataset = muni[year]
+def handle_sheet(year, filename):
+    dataset = Dataset(MUNI, year)
     reader = csv.DictReader(file(filename, 'rb'), fields)
 
     for line in reader:
         print "%s : %s" %(line['code'], line['amount'])
         if (line['name'] != '' and line['amount'].isdigit()):
-            new_line  = {'name':line['name'], 'amount':line['amount'], 'code':line['code'] }
-            year_dataset.insert(new_line);
-    client.close() 
-    
+            new_line  = {'n    ame':line['name'], 'amount':line['amount'], 'code':line['code'] }
+            dataset.insert(new_line);
+    dataset.close()
