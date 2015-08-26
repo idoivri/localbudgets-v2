b52517473aaf81cff4f3fd52b9b5c2f7740fcdba
inbar naor <inabr.naor1@gmailcom>
2015-08-26 22:09:59 +0300

    update rishon letzion

--- upload/muni/rishon_letzion.py
+++ upload/muni/rishon_letzion.py
@@ -1,7 +1,12 @@
 from muni import AbstractMuni
+from fields import AmountField, CodeField, DescriptionField
 
 class RishonLetzionMuni(AbstractMuni):
+
+    fields = {0: CodeField,
+              1: DescriptionField,
+              4: AmountField}
+
     MUNI = 'rishon_letzion'
-    def handle_sheet(*args):
-	    pass
+
 
