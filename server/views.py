aed2ab8d8bb9a053e82b875f70173f15c15ff9be
mestrebisli <mestrebisli@gmail.com>
2015-10-21 20:58:54 +0300

    started creating new index page

--- server/views.py
+++ server/views.py
@@ -4,34 +4,47 @@ from pymongo import MongoClient
 from pymongo import MongoClient as client
 from django.shortcuts import render
 
-from server.models import get_budget
+from server.models import get_budget, get_munis
+
+from collections import defaultdict
 
 def show_table(request, muni_name,year):
-    client = MongoClient()
+    # client = MongoClient()
     # db = client.database
     muni = get_budget(muni_name,year)
     lines = []
     for line in muni.find():
         lines.append(line)
-    
+
     muni.close()
     return render(request, 'simple_table.html', {'query_results':lines} )
 
+# def
+
 def index_page(request):
-    res = []
+    # res = []
+    #
+    # muni1 = {}
+    # muni1['id'] = 2
+    # muni1['active'] = ''
+    # muni1['value'] = 'dynamic'
+    # muni1['heb_name'] = u'בזרימה'
+    # res.append(muni1)
+    #
+    # muni = {}
+    # muni['id'] = 1
+    # muni['active'] = 'active'
+    # muni['value'] = 'standard'
+    # muni['heb_name'] = u'חיפוש רגיל'
+    # res.append(muni)
+    # client = muni_
 
-    muni1 = {}
-    muni1['id'] = 2
-    muni1['active'] = ''
-    muni1['value'] = 'dynamic'
-    muni1['heb_name'] = u'בזרימה'
-    res.append(muni1)
+    res = defaultdict(list)
+    for muni,year in get_munis():
+        res[muni].append(year)
 
-    muni = {}
-    muni['id'] = 1
-    muni['active'] = 'active'
-    muni['value'] = 'standard'
-    muni['heb_name'] = u'חיפוש רגיל'
-    res.append(muni)
+    for muni in res:
+        res[muni].sort()
 
-    return render(request,'index.html', {'munis_result':res})
+    # muni.close()
+    return render(request,'index.html', {'munis': res.keys() , 'years':res})
