33c53d5c27f650a46bc66fb76f0225f0dcc59780
Nitzan Guberman <nitzankroyzer@gmail.com>
2016-06-15 18:49:40 +0000

    added hebrew names

--- upload/muni/muni_info.py
+++ upload/muni/muni_info.py
@@ -1,15 +1,17 @@
+# -*- coding: utf-8 -*-
+
 
 defualt_info = {'heb_name': 'bla'}
 
-infos = {'jerusalem': defualt_info,
-         'ashdod': defualt_info,
-         'beer_sheva': defualt_info,
-         'gush_etzion': defualt_info,
-         'hura': defualt_info,
-         'kfar_shmaryahu': defualt_info,
-         'qiryat_bialik': defualt_info,
-         'rehovot': defualt_info,
-         'rishon_letzion': defualt_info,
-         'tel_aviv': defualt_info,
-         'schema': defualt_info
+infos = {'jerusalem': {'heb_name': u'רושלים'},
+         'ashdod': {'heb_name': u'אשדוד'},
+         'beer_sheva': {'heb_name': u'באר שבע'},
+         'gush_etzion': {'heb_name': u'גוש עציון'},
+         'hura': {'heb_name': u'חורה'},
+         'kfar_shmaryahu': {'heb_name': u'כפר שמריהו'},
+         'qiryat_bialik': {'heb_name': u'קרית ביאליק'},
+         'rehovot': {'heb_name': u'רחובות'},
+         'rishon_letzion': {'heb_name': u'ראשון לציון'},
+         'tel_aviv': {'heb_name': u'תל אביב'},
+         'schema': {}
          }
