fad6744d51de2948f4f6f1aab544d0a213176b7f
Avi Illouz <avi.illouz@gmail.com>
2017-01-10 20:31:17 +0200

    wrap try

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -340,16 +340,19 @@ function get_data(muni,year,expense) {
       var entering = g.enter().append("svg:g");
 
       entering.append("svg:polygon")
+          .attr("class","breadcrumb")
           .attr("points", breadcrumbPoints)
           .style("fill", function(d) { return getColor(d) });
 
       entering.append("svg:text")
+          .attr('class','breadcrumb-text')
           .attr("x", (b.w + b.t) / 2)
           .attr("y", b.h / 2)
           .attr("dy", "0.35em")
-          .attr("text-anchor", "middle")
+          .attr("text-anchor", "top")
           .text(function(d) { return d.name; })
-          .style('fill','white'); //TODO color should be white
+          .style('fill','black'); //TODO color should be white
+          
 
       // Set position for entering and updating nodes.
       g.attr("transform", function(d, i) {
@@ -367,11 +370,42 @@ function get_data(muni,year,expense) {
           .attr("text-anchor", "middle")
           .text(percentageString);
 
+          d3plus.textwrap()
+             .container(d3.select(".breadcrumb"))
+             .resize(true)
+             .draw()
+
+          // d3.selectAll(".breadcrumb-text")
+          // .call(wrap, 30)
       // Make the breadcrumb trail visible, if it's hidden.
       d3.select("#trail")
           .style("visibility", "");
     }
   }
+
+  function wrap(text, width) {
+  text.each(function() {
+    var text = d3.select(this),
+        words = text.text().split(/\s+/).reverse(),
+        word,
+        line = [],
+        lineNumber = 0,
+        lineHeight = 1.1, // ems
+        y = text.attr("y"),
+        dy = parseFloat(text.attr("dy")),
+        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
+    while (word = words.pop()) {
+      line.push(word);
+      tspan.text(line.join(" "));
+      if (tspan.node().getComputedTextLength() > width) {
+        line.pop();
+        tspan.text(line.join(" "));
+        line = [word];
+        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+      }
+    }
+  });
+}
 
   $(document).ready(function() {
     // Initialize Bootstrap Switch
