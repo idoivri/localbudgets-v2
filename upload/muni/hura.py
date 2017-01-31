6becf0bfd2668c28c43d1ccfac0ce0e8c744c68d
oferfrid <oferfrid@gmail.com>
2017-01-31 21:23:06 +0200

    move data no new coll * 1000

--- upload/muni/hura.py
+++ upload/muni/hura.py
@@ -4,7 +4,7 @@ from fields import AmountField, CodeField, DescriptionField
 class HuraMuni(AbstractMuni):
     fields = {0: CodeField,
               1: DescriptionField,
-              6: AmountField}  # TODO: make sure this is the right column number.
+              7: AmountField}  # TODO: make sure this is the right column number.
 
     MUNI = 'hura'
 
