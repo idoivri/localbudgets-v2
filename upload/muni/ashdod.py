7770943c68ec3ebd8702fe7d3d80cb0bed2146d2
oferfrid <oferfrid@gmail.com>
2017-03-07 23:06:28 +0200

    add ashdod and fix bug in start in row

--- upload/muni/ashdod.py
+++ upload/muni/ashdod.py
@@ -4,12 +4,15 @@ from fields import AmountField, CodeField, DescriptionField
 
 
 class AshdodMuni(AbstractMuni):
-    fields = ['code', 'name', 'amount']
     fields = {0: CodeField,
               1: DescriptionField,
               2: AmountField}
-
+    fields_2016 = {1: CodeField,
+              2: DescriptionField,
+              7: AmountField}
     MUNI = 'ashdod'
-
+    years = {2016: fields_2016}
     def __init__(self,**kwargs):
+        self.start_in_row.add_value(0)
+        self.start_in_row.add_value(1,year=2016)
         super(AshdodMuni,self).__init__(**kwargs)
