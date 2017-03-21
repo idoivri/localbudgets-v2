8ff607234e8a7af0ba5e83ff4a3d61a4e964fe88
oferfrid <oferfrid@gmail.com>
2017-03-21 21:08:11 +0200

    add ashdod and fix bug in start in row

--- upload/muni/ashdod.py
+++ upload/muni/ashdod.py
@@ -1,8 +1,6 @@
 from muni import AbstractMuni
 from fields import AmountField, CodeField, DescriptionField
 
-
-
 class AshdodMuni(AbstractMuni):
     fields = {0: CodeField,
               1: DescriptionField,
