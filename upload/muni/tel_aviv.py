14a058a63e3c058452a95fc9a2aa8b4e41d2ecff
oferfrid <oferfrid@gmail.com>
2017-06-20 22:25:21 +0300

    add TA

--- upload/muni/tel_aviv.py
+++ upload/muni/tel_aviv.py
@@ -17,4 +17,17 @@ class TelAvivMuni(AbstractMuni):
               6: DescriptionField,
               12: AmountField},2014)
         self.start_in_row.add_value(8,year=2014)
+        self.data_fields.add_value({3: CodeField,
+                                    4: DescriptionField,
+                                    5: AmountField}, 2013)
+        self.start_in_row.add_value(1, year=2013)
+        self.data_fields.add_value({3: CodeField,
+                                    4: DescriptionField,
+                                    6: AmountField}, 2012)
+        self.start_in_row.add_value(1, year=2012)
+        self.data_fields.add_value({3: CodeField,
+                                4: DescriptionField,
+                                7: AmountField}, 2011)
+        self.start_in_row.add_value(1, year=2011)
+
 
