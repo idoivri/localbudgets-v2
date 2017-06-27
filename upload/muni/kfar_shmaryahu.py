8e043499eed64822c136406460c06f63c71613d6
oferfrid <oferfrid@gmail.com>
2017-06-27 22:02:39 +0300

    remove kfar shmaryahu where data was in utf-8 but with ??? insted of hebrew

--- upload/muni/kfar_shmaryahu.py
+++ upload/muni/kfar_shmaryahu.py
@@ -1,12 +1,13 @@
 from muni import AbstractMuni
+from fields import AmountField, CodeField, DescriptionField
 
 class KfarShmaryahuMuni(AbstractMuni):
     MUNI = 'kfar_shmaryahu'
-
-    def handle_sheet(*args):
-        # TODO
-        pass
-
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         super(KfarShmaryahuMuni, self).__init__(**kwargs)
-        self.start_in_row.add_value(3)
+        self.data_fields.add_value({0: CodeField,
+                                    2: DescriptionField,
+                                    6: AmountField})
+        self.start_in_row.add_value(1)
+
+
