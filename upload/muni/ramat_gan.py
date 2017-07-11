5ed98c1097cd74ed8eb0974b4c9593f69d363be3
nitzanguberman <nitzankroyzer@gmail.com>
2017-07-11 21:04:20 +0300

    Add 1 to code field

--- upload/muni/ramat_gan.py
+++ upload/muni/ramat_gan.py
@@ -5,7 +5,7 @@ class RamatGanMuni(AbstractMuni):
     MUNI = 'ramat_gan'
     def __init__(self,**kwargs):
         super(RamatGanMuni, self).__init__(**kwargs)
-        self.data_fields.add_value({0: CodeField,
+        self.data_fields.add_value({1: CodeField,
               5: DescriptionField,
               6: AmountField})
         self.start_in_row.add_value(1)
